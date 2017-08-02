23266ad2077a5bf07a18e4e25a81729a77fa4685
lucalan66 <lucalan66@gmail.com>
2017-08-01 23:15:40 -0500

    buscando nodos

--- xml.rb
+++ xml.rb
@@ -1,11 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
-@doc = Nokogiri::HTML(open("https://www.myvoicecomm.com/portal/api/itemget_form.php?api=InventoryRequest&AccountNumber=13358&AccountPassword=485zmlan01&CategoryID=132&Status=1&Format=XML "))
+@doc = Nokogiri::HTML(open("https://www.myvoicecomm.com/portal/api/itemget_form.php?api=InventoryRequest&AccountNumber=13358&AccountPassword=485zmlan01&CategoryID=&Status=1&Format=XML"))
+
 
 item = @doc.css("item")
-
-p item
-
 
 item.each do |item|
 
